27264c957fa1c90695a4832604cc24c8d2394729
srilakshman73 <srilakshman73@gmail.com>
2026-09-05 00:53:24 +0530

    Fix static files serving for Vercel deployment

--- bairava_project/urls.py
+++ bairava_project/urls.py
@@ -2,9 +2,10 @@
 URL configuration for bairava_project project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,11 +14,10 @@ urlpatterns = [
     path('legal/', include('law_associates.urls')),
     path('events/', include('events.urls')),
     path('', include('core.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
 admin.site.site_header = "Bairava Groups Admin Portal"
 admin.site.site_title = "Bairava Groups"
 admin.site.index_title = "Welcome to Bairava Groups Management"
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 python3 -m pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput --clear
+mkdir -p staticfiles_build/static
+cp -r staticfiles/* staticfiles_build/static/ 2>/dev/null || true
